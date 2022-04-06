d1c643f66252a5d91abf6129a621d4525ed5091a
tozggg <tozggg@gmail.com>
2022-04-06 21:15:01 +0900

    alter check, parse info/map add

--- main.c
+++ main.c
@@ -6,16 +6,36 @@
 /*   By: taejkim <taejkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 13:49:34 by taejkim           #+#    #+#             */
-/*   Updated: 2022/03/31 16:00:11 by taejkim          ###   ########.fr       */
+/*   Updated: 2022/04/06 21:07:07 by taejkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "./minilibx/mlx.h"
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/types.h>
+#include <math.h>
 
+#define HEIGHT 960
+#define WIDTH 640
+#define TEX_SIZE 64
+
+#define X_EVENT_KEY_PRESS 2
+#define X_EVENT_KEY_RELEASE 3
+#define X_EVENT_KEY_EXIT 17
+
+#define KEY_W 13
+#define KEY_A 0
+#define KEY_S 1
+#define KEY_D 2
+#define KEY_LEFT 123
+#define KEY_RIGHT 124
+#define KEY_UP 126
+#define KEY_DOWN 125
+#define KEY_ESC 53
 
 #define NO 0
 #define SO 1
@@ -23,6 +43,7 @@
 #define EA 3
 #define F 4
 #define C 5
+
 
 
 typedef struct s_file
@@ -36,8 +57,48 @@ typedef struct s_file
 	int		c_flag;
 }	t_file;
 
+typedef struct s_key
+{
+	int		w;
+	int		a;
+	int		s;
+	int		d;
+	int		left;
+	int		right;
+}	t_key;
+
+typedef struct s_img
+{
+	void	*img;
+	int		*data;
+	int		size_l;
+	int		bpp;
+	int		endian;
+	int		width;
+	int		height;
+}	t_img;
+
 typedef struct s_game
 {
+	void	*mlx;
+	void	*win;
+	t_key	key;
+	t_img	img;
+	int		**buf;
+	int		*texture[4];
+	int		ceiling;
+	int		floor;
+	char	**map;
+	int		map_w;
+	int		map_h;
+	double	pos_x;
+	double	pos_y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+	double	movespeed;
+	double	rotspeed;
 }	t_game;
 
 
@@ -240,6 +301,14 @@ void	*ft_calloc(size_t count, size_t size)
 	ft_bzero(ptr, (count * size));
 	return (ptr);
 }
+
+int		ft_isdigit(int c)
+{
+	if ('0' <= c && c <= '9')
+		return (1);
+	return (0);
+}
+
 ///////////////////////////////////////////////////////////////////
 
 //utils
@@ -286,6 +355,51 @@ char	*read_file(char *pathname)
 	return (res);
 }
 
+int	is_dir_flag(char c)
+{
+	if (c == 'N' || c == 'S' || c == 'W' || c == 'E')
+		return (1);
+	return (0);
+}
+
+char	*cut_path(char *str, char *cut)
+{
+	while (*cut && *str)
+	{
+		if (*cut != *str)
+			return (NULL);
+		++cut;
+		++str;
+	}
+	while (*str == ' ')
+		++str;
+	return (str);
+}
+
+int	cut_rgb(char *str)
+{
+	int res;
+
+	if (!ft_isdigit(*str))
+		return (-1);
+	res = 0;
+	while (*str && ft_isdigit(*str))
+	{
+		res = (res * 10) + (*str - '0');
+		++str;
+	}
+	while (*str == ' ')
+		++str;
+	if (*str == ',')
+		++str;
+	while (*str == ' ')
+		++str;
+	if (0 <= res && res <= 255)
+		return (res);
+	else
+		return (-1);
+}
+
 //init
 void	init_file(t_file *file)
 {
@@ -300,6 +414,44 @@ void	init_file(t_file *file)
 	file->map_w = 0;
 	file->map_h = 0;
 	file->c_flag = 0;
+}
+
+int	**init_buf(void)
+{
+	int	**res;
+	int	*tmp;
+	int	i;
+
+	res = (int **)malloc(sizeof(int *) * HEIGHT);
+	if (!res)
+		error_out("Error\nmalloc error");
+	i = 0;
+	while (i < HEIGHT)
+	{
+		tmp = (int *)ft_calloc(WIDTH, sizeof(int));
+		if (!tmp)
+			error_out("Error\nmalloc error");
+		res[i] = tmp;
+		++i;
+	}
+	return (res);
+}
+
+void	init_game(t_game *game)
+{
+	game->mlx = mlx_init();
+	game->key.w = 0;
+	game->key.s = 0;
+	game->key.a = 0;
+	game->key.d = 0;
+	game->key.left = 0;
+	game->key.right = 0;
+	game->img.img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
+	game->img.data = (int *)mlx_get_data_addr(game->img.img, &game->img.bpp, \
+										&game->img.size_l, &game->img.endian);
+	game->buf = init_buf();
+	game->movespeed = 0.05;
+	game->rotspeed = 0.05;
 }
 
 //check
@@ -357,51 +509,45 @@ void	append_map(t_file *file)
 	int	i;
 	int	j;
 
-	file->map = (char **)malloc(sizeof(char *) * (file->map_h + 3));
+	file->map = (char **)malloc(sizeof(char *) * (file->map_h + 1));
 	i = 0;
-	file->map[i] = ft_calloc(file->map_w + 3, sizeof(char));
-	ft_memset(file->map[i++], ' ', file->map_w + 2);
-	while (i <= file->map_h)
+	while (i < file->map_h)
 	{
-		file->map[i] = ft_calloc(file->map_w + 3, sizeof(char));
+		file->map[i] = (char *)ft_calloc(file->map_w + 1, sizeof(char));
 		j = 0;
-		file->map[i][j++] = ' ';
-		while (file->split[i + 5][j - 1])
+		while (file->split[i + 6][j])
 		{
-			file->map[i][j] = file->split[i + 5][j - 1];
+			file->map[i][j] = file->split[i + 6][j];
 			j++;
 		}
-		while (j <= file->map_w + 1)
+		while (j < file->map_w)
 			file->map[i][j++] = ' ';
 		file->map[i][j] = 0;
 		++i;
 	}
-	file->map[i] = ft_calloc(file->map_w + 3, sizeof(char));
-	ft_memset(file->map[i++], ' ', file->map_w + 2);
 	file->map[i] = 0;
-}
-
-int		is_dir_flag(char c)
-{
-	if (c == 'N' || c == 'S' || c == 'W' || c == 'E')
-		return (1);
-	return (0);
 }
 
 void	check_surround(t_file *file, int y, int x)
 {
-	if (x != 0 && !(file->map[y][x - 1] == '1' \
-				|| file->map[y][x - 1] == ' '))
-		error_out("Error\ninvalid map");
-	if (y != 0 && !(file->map[y - 1][x] == '1' \
-				|| file->map[y - 1][x] == ' '))
-		error_out("Error\ninvalid map");
-	if (x != file->map_w + 1 && !(file->map[y][x + 1] == '1' \
-				|| file->map[y][x + 1] == ' '))
-		error_out("Error\ninvalid map");
-	if (y != file->map_h + 1 && !(file->map[y + 1][x] == '1' \
-				|| file->map[y + 1][x] == ' '))
-		error_out("Error\ninvalid map");
+	if ((x == 0 || x == file->map_w - 1 || y == 0 || y == file->map_h - 1) \
+		&& !(file->map[y][x] == '1' || file->map[y][x] == ' '))
+			error_out("Error\ninvalid map");
+	if (file->map[y][x] == ' ')
+	{
+		if (x != 0 && !(file->map[y][x - 1] == '1' \
+					|| file->map[y][x - 1] == ' '))
+			error_out("Error\ninvalid map");
+		if (y != 0 && !(file->map[y - 1][x] == '1' \
+					|| file->map[y - 1][x] == ' '))
+			error_out("Error\ninvalid map");
+		if (x != file->map_w - 1 && !(file->map[y][x + 1] == '1' \
+					|| file->map[y][x + 1] == ' '))
+			error_out("Error\ninvalid map");
+		if (y != file->map_h - 1 && !(file->map[y + 1][x] == '1' \
+					|| file->map[y + 1][x] == ' '))
+			error_out("Error\ninvalid map");
+	}
 }
 
 void	check_map(t_file *file)
@@ -424,14 +570,81 @@ void	check_map(t_file *file)
 				else
 					error_out("Error\ninvalid map");
 			}
-			if (file->map[i][j] == ' ')
-				check_surround(file, i , j);
+			check_surround(file, i , j);
 		}
 	}
 	if (!file->c_flag)
 		error_out("Error\ninvalid map");
 }
 
+
+//parse
+int	*load_texture(t_game *game, char *path, t_img *img)
+{
+	int	*res;
+	int	x;
+	int	y;
+
+	img->img = mlx_xpm_file_to_image(game->mlx, path, &img->width, &img->height);
+	if (!img->img || img->width != TEX_SIZE || img->height != TEX_SIZE)
+		error_out("Error\ninvalid texture");
+	img->data = (int *)mlx_get_data_addr(img->img, &img->bpp, &img->size_l, &img->endian);
+	res = (int *)malloc(sizeof(int) * (TEX_SIZE * TEX_SIZE));
+	y = -1;
+	while (++y < TEX_SIZE)
+	{
+		x = -1;
+		while (++x < TEX_SIZE)
+			res[y * TEX_SIZE + x] = img->data[y * TEX_SIZE + x];
+	}
+	mlx_destroy_image(game->mlx, img->img);
+	return (res);
+}
+
+int	get_rgb(char *str)
+{
+	int r;
+	int g;
+	int b;
+
+	r = cut_rgb(str);
+	g = cut_rgb(str);
+	b = cut_rgb(str);
+	if (r == -1 || g == -1 || b == -1)
+		error_out("Error\ninvalid rgb");
+	return ((r * 256 * 256) + (g * 256) + b);
+}
+
+void	parse_info(t_game *game, t_file file)
+{
+	t_img	tmp;
+	char	*path;
+
+	path = cut_path(file.info[NO], "NO");
+	game->texture[NO] = load_texture(game, path, &tmp);
+	path = cut_path(file.info[SO], "SO");
+	game->texture[SO] = load_texture(game, path, &tmp);
+	path = cut_path(file.info[WE], "WE");
+	game->texture[WE] = load_texture(game, path, &tmp);
+	path = cut_path(file.info[EA], "EA");
+	game->texture[EA] = load_texture(game, path, &tmp);
+	path = cut_path(file.info[C], "C");
+	game->ceiling = get_rgb(path);
+	path = cut_path(file.info[F], "F");
+	game->floor = get_rgb(path);
+}
+
+void	make_game(t_game *game, t_file file)
+{
+	init_game(game);
+	parse_info(game, file);
+	game->map = file.map;
+	game->map_w = file.map_w;
+	game->map_h = file.map_h;
+}
+
+
+//main
 void	check_file(t_file *file, char *path)
 {
 	init_file(file);
@@ -441,13 +654,8 @@ void	check_file(t_file *file, char *path)
 		error_out("Error\nsplit error");
 	check_info(file);
 	check_map(file);
-
-	//
-	printf("kk");
 }
 
-
-//main
 int main(int ac, char *av[])
 {
 	t_file	file;
@@ -456,7 +664,8 @@ int main(int ac, char *av[])
 	if (ac != 2 || !is_cub_extension(av[1]))
 		error_out("Error\nusage: ./cub3d [map.cub]");
 	check_file(&file, av[1]);
-		
+	make_game(&game, file);
+
 
 	return (0);
 }
